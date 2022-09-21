46dd2cb50179e334be172627649756f19d28163a
Virginia <94895896+virginia-oc@users.noreply.github.com>
2022-09-21 12:07:45 +0200

    Instancio disparo sin movimiento

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
-    [SerializeField] float velocidad = 2;    
+    [SerializeField] float velocidad = 2;
+    [SerializeField] Transform prefabDisparo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,11 @@ public class Nave : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         transform.Translate(horizontal * velocidad * Time.deltaTime, 0, 0);
         //Debug.Log(Time.deltaTime + "seg, " + (1.0f / Time.deltaTime) + "FPS");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(prefabDisparo, transform.position, Quaternion.identity);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
